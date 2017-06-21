62f158664f2993d80af3590f2fb0b40fcc2fae26
Sylvain Leroux <sylvain@chicoree.fr>
2017-06-21 16:45:04 +0200

    Basically working version

--- test/test.js
+++ test/test.js
@@ -1,9 +1,46 @@
-var assert = require('assert');
+const assert = require('chai').assert;
+const cheerio = require('cheerio');
+
 
 console.log(__dirname);
 
 describe("module loading", function() {
-  it('shoud load the module', function() {
-    var module = require('../index.js');
+  it('should load the module', function() {
+    const module = require('../index.js');
   });
+});
+
+
+describe("library", function() {
+  const htt = require('../index.js');
+
+  describe("convert()", function() {
+    const html = "<html><body><p>hello</p></body></html>";
+
+    it('should return a string', function() {
+      const a = htt.convert(html);
+      assert.isString(a);
+    });
+
+    it('should produce the same result given string or cheerio object', function() {
+      const a = htt.convert(html);
+      const b = htt.convert(cheerio.load(html));
+      assert.isDefined(a);
+      assert.equal(a,b);
+    });
+
+    it('should properly join inline elements', function() {
+      const html = "<span>Hello <i>world</i><strong>!</strong></span>";
+      const a = htt.convert(html);
+      assert.equal("Hello world!", a);
+    });
+
+    it('should handle text-only fragments', function() {
+      const html = "Hello world!";
+      const a = htt.convert(html);
+      assert.equal("Hello world!", a);
+    });
+
+  });
+
 });
